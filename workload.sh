447365c217dd45a3ff9f95f493d0f5e5bd948908
Tatzhiro <64573463+Tatzhiro@users.noreply.github.com>
2023-01-07 16:09:03 +0900

    refactor scraping

--- workload.sh
+++ workload.sh
@@ -1,3 +1,5 @@
+set -xu
+
 NUM_TABLES=${5}
 TABLE_SIZE=${6}
 NUM_THREADS=${7}
@@ -6,9 +8,6 @@ OLTP_WORK=${9}
 INNO_BUF_SIZE=${10}
 INNO_IO_CAP=${11}
 FILENAME=${12}
-
-
-
 
 
 sysbench --db-driver=mysql --mysql-host=$1 --mysql-user=$2 --mysql-password=$3 --mysql-db=$4 --tables=${NUM_TABLES} --table_size=${TABLE_SIZE} --threads=${NUM_THREADS} --time=${TEST_DURATION} ${OLTP_WORK} run
