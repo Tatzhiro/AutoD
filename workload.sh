5386188f6e31afb8e99b63a3cdca61439522c034
Tatzhiro <64573463+Tatzhiro@users.noreply.github.com>
2023-01-20 22:13:14 +0900

    test

--- workload.sh
+++ workload.sh
@@ -9,6 +9,6 @@ INNO_BUF_SIZE=${10}
 INNO_IO_CAP=${11}
 FILENAME=${12}
 
-
+mysql -h ${1} -u ${2} -p${3} -e "set global innodb_buffer_pool_size=${INNO_BUF_SIZE}; set global innodb_io_capacity=${INNO_IO_CAP}"
 sysbench --db-driver=mysql --mysql-host=$1 --mysql-user=$2 --mysql-password=$3 --mysql-db=$4 --tables=${NUM_TABLES} --table_size=${TABLE_SIZE} --threads=${NUM_THREADS} --time=${TEST_DURATION} ${OLTP_WORK} run
 python3 exporter.py --configs label=${OLTP_WORK} tablesize=${TABLE_SIZE} -f ${FILENAME}
