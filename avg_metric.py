c076141e56ccb6ea2f7c6064ae79b955113c0876
Ubuntu <azureuser@db.wahnmborhx0urpiaoaxjggz50g.lx.internal.cloudapp.net>
2023-03-17 01:18:39 +0000

    calculate threshold by average over 1 minute

--- avg_metric.py
+++ avg_metric.py
@@ -1,10 +1,17 @@
 import pandas as pd
 import argparse
 
+def calculate_num_rows():
+  minutes = args.time * 60
+  step_second = args.steps
+  num_rows = minutes / step_second
+  return num_rows
+
 def average_column(filename, column_name):
   df = pd.read_csv(filename)
   column = df[column_name]
-  average = column.mean()
+  rows = calculate_num_rows()
+  average = column.loc[0:rows-1].mean()
   return average
 
 
@@ -18,11 +25,21 @@ if __name__ == "__main__":
                       action='store',
                       type=str,
                       default="metrics.csv",
-                      help='出力先のファイル名を指定します')
+                      help='読み込むファイル名を指定します')
   parser.add_argument('-c', '--clmname',
                       action='store',
                       type=str,
                       default="disk IOPS",
                       help='column name')
+  parser.add_argument('-s', '--steps',
+                      action='store',
+                      type=int,
+                      default=5,
+                      help='interval of datapoints in seconds')
+  parser.add_argument('-t', '--time',
+                      action='store',
+                      type=int,
+                      default=1,
+                      help='average over how many minutes')
   args = parser.parse_args()
   main()
